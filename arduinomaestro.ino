53ce9af2e8281a0198027068db45394b02446aea
tob22297 <tob22297@uvg.edu.gt>
2024-05-31 11:24:58 -0600

    Definición de pines de switch y mensajes

--- arduinomaestro.ino
+++ arduinomaestro.ino
@@ -8,3 +8,11 @@ LiquidCrystal_I2C lcd (0x27, 16, 2);
 typedef enum estados {e0, e1, e2} estados;
 estados estadoActual = e0;
 estados estadoFuturo = e0;
+
+//pines de switch
+const uint8_t boton1 = 12;
+const uint8_t boton2 = 13;
+
+// mensajes
+String aviso1 = "Falta tiro libre";
+String aviso2 = "Patea";
