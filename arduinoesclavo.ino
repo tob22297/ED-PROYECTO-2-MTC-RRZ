64d727fca1cc76ea3075fbe7303be3abd56b8559
rod22803 <rod22803@uvg.edu.gt>
2024-05-31 13:05:22 -0600

    Creación del loop en el esclavo

--- arduinoesclavo.ino
+++ arduinoesclavo.ino
@@ -11,3 +11,15 @@ void setup() {
   servomotor1.attach(9);
   servomotor2.attach(10);
 }
+void loop() {
+  if (Serial.available() > 0) {
+    // Leer datos del puerto serie
+    int ang1Recibido = Serial.parseInt(); // Lee el primer número
+    int ang2Recibido = Serial.parseInt(); // Lee el segundo número
+
+    // Controlar los servomotores
+    servomotor1.write(ang1Recibido);
+    servomotor2.write(ang2Recibido);
+  }
+  delay(100);
+}
